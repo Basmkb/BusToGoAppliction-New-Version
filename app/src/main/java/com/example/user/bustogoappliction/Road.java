b4030b19382554f4b416b3969d44a404ab740d4c
Basmkb <basgenz11@gmail.com>
2017-10-03 17:47:45 +0700

    อัพเดท ข้อมูลใส่ไปเเล้ว

--- app/src/main/java/com/example/user/bustogoappliction/Road.java
+++ app/src/main/java/com/example/user/bustogoappliction/Road.java
@@ -14,7 +14,7 @@ public class Road extends AppCompatActivity {
     private PlaceTable objPlaceTable;
     private BusTale objBusTale;
     ListView listView;
-    private String[]titles_bus,detail_bus,image_bus;
+    private String[]titles_bus,detail_bus,image_bus,latitude,longitude;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,9 +31,30 @@ public class Road extends AppCompatActivity {
         final String[] titles = objPlaceTable.readAllPlace(1);
         final String[] detail = objPlaceTable.readAllPlace(2);
         final String[] image = objPlaceTable.readAllPlace(5);
+        final String[] latt = objPlaceTable.readAllPlace(3);
+        final String[] longg = objPlaceTable.readAllPlace(4);
+
 
         MyadpterRoad myadpter = new MyadpterRoad(Road.this, image, titles, detail);
         listView.setAdapter(myadpter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {//คลิกจากหน้า Road จะแสดงข้อมูลการเดินทาง
+                titles_bus = titles;//เก็บค่าตัวแปรจากจาก SQL
+                detail_bus = detail;//เก็บค่าตัวแปรจากจาก SQL
+                image_bus = image;//เก็บค่าตัวแปรจากจาก SQL
+                latitude = latt;//เก็บค่าตัวแปรจากจาก SQL
+                longitude = longg;//เก็บค่าตัวแปรจากจาก SQL
+                Intent intent = new Intent(Road.this, GiantSwing.class);//ส่งค่าข้อมูลจากดาต้าเบสไปหน้า GiantSwing
+                intent.putExtra("title", titles[position]);
+                intent.putExtra("detail", detail[position]);
+                intent.putExtra("image", image[position]);
+                intent.putExtra("lat", latt[position]);
+                intent.putExtra("long", longg[position]);
+                startActivity(intent);
+            }
+        });
+
 
     }
 
